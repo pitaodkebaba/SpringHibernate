17b84afac24fd352045c479283d3f10916a5d3b1
Pomaranczarz <pomaranczarz@gmail.com>
2024-12-02 16:21:03 +0100

    Added custom password encoder with salt and pepper

---
+++ src/main/java/com/example/project/Backend/Services/CustomPasswordEncoder.java
@@ -0,0 +1,24 @@
+package com.example.project.Backend.Services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomPasswordEncoder implements PasswordEncoder {
+    @Value("${security.pepper}")
+    private String pepper;
+
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return encoder.encode(pepper + rawPassword);
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return encoder.matches(pepper + rawPassword, encodedPassword);
+    }
+}
